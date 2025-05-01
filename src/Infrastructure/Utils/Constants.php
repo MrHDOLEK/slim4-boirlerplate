64488aaa275fb2d565f57700849979372468ed08
Aleksander Kowalski <mrhdolek14@gmail.com>
2025-05-01 13:19:42 +0200

    fix: cs-fix

--- src/Infrastructure/Utils/Constants.php
+++ src/Infrastructure/Utils/Constants.php
@@ -6,7 +6,7 @@ namespace App\Infrastructure\Utils;
 
 class Constants
 {
-    public const ONE_HOUR = 3600;
+    public const ONE_HOUR = 1 * 60 * 60;
     public const CONSOLE_ROUTE = "/bin/console.php";
     public const ARGUMENT_SEPARATOR = "_";
     public const int MAX_RETRY_COUNT = 3;
