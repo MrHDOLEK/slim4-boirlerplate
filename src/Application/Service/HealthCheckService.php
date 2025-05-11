d57edaba460cf625d79067923cfd2a3362cce301
Aleksander Kowalski <mrhdolek14@gmail.com>
2025-05-11 12:55:08 +0200

    #14 - refactor docker-compose and health check (#46)

--- src/Application/Service/HealthCheckService.php
+++ src/Application/Service/HealthCheckService.php
@@ -4,23 +4,84 @@ declare(strict_types=1);
 
 namespace App\Application\Service;
 
+use App\Infrastructure\AMQP\AMQPStreamConnectionFactory;
+use Doctrine\ORM\EntityManagerInterface;
+use Predis\Client as RedisClient;
+use Psr\Log\LoggerInterface;
+
 class HealthCheckService
 {
     public const STATUS_OK = "OK";
     public const STATUS_ERROR = "ERROR";
 
-    private int $statusCode = 200;
+    private int $statusCode;
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private RedisClient $redisClient,
+        private AMQPStreamConnectionFactory $rabbitMqClient,
+        private LoggerInterface $logger,
+    ) {}
 
     public function statusList(): array
     {
-        return [
+        $statuses = [
+            "DB_CONNECTION" => $this->databaseStatus(),
             "API_CONNECTION" => $this->apiStatus(),
+            "REDIS_CONNECTION" => $this->redisStatus(),
+            "RABBITMQ_CONNECTION" => $this->rabbitMqStatus(),
         ];
+
+        $this->logger->info(
+            "Health check statuses",
+            array_change_key_case($statuses, CASE_LOWER),
+        );
+
+        return $statuses;
     }
 
     public function statusCode(): int
     {
         return $this->statusCode;
+    }
+
+    public function databaseStatus(): string
+    {
+        if (!$this->entityManager->isOpen()) {
+            $this->statusCode = 503;
+
+            return self::STATUS_ERROR;
+        }
+
+        $this->statusCode = 200;
+
+        return self::STATUS_OK;
+    }
+
+    public function redisStatus(): string
+    {
+        if (!((string)$this->redisClient->ping()->getPayload() === "PONG")) {
+            $this->statusCode = 503;
+
+            return self::STATUS_ERROR;
+        }
+
+        $this->statusCode = 200;
+
+        return self::STATUS_OK;
+    }
+
+    public function rabbitMqStatus(): string
+    {
+        if (!$this->rabbitMqClient->get()->isConnected()) {
+            $this->statusCode = 503;
+
+            return self::STATUS_ERROR;
+        }
+
+        $this->statusCode = 200;
+
+        return self::STATUS_OK;
     }
 
     public function apiStatus(): string
--- tests/Application/Actions/HealthCheck/HealthCheckActionTest.php
+++ tests/Application/Actions/HealthCheck/HealthCheckActionTest.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Tests\Application\Actions\HealthCheck;
 
 use App\Application\Service\HealthCheckService;
-use DI\Container;
-use PHPUnit\Framework\MockObject\MockObject;
 use Tests\TestCase;
 
 class HealthCheckActionTest extends TestCase
@@ -14,29 +12,39 @@ class HealthCheckActionTest extends TestCase
     public function testHealthCheckActionSuccess(): void
     {
         $app = $this->getApp();
-        /** @var Container $container */
         $container = $app->getContainer();
 
-        /** @var HealthCheckService&MockObject $healthCheckServiceMock */
         $healthCheckServiceMock = $this->createMock(HealthCheckService::class);
-        $healthCheckServiceMock
-            ->expects($this->once())
-            ->method("statusCode")
-            ->willReturn(200);
+
         $healthCheckServiceMock
             ->expects($this->once())
             ->method("statusList")
             ->willReturn([
+                "DB_CONNECTION" => "OK",
                 "API_CONNECTION" => "OK",
+                "REDIS_CONNECTION" => "OK",
+                "RABBITMQ_CONNECTION" => "OK",
             ]);
+
+        $healthCheckServiceMock
+            ->expects($this->once())
+            ->method("statusCode")
+            ->willReturn(200);
 
         $container->set(HealthCheckService::class, $healthCheckServiceMock);
 
         $request = $this->createRequest("GET", "/health-check");
         $response = $app->handle($request);
 
+        $expectedResponseBody = [
+            "DB_CONNECTION" => "OK",
+            "API_CONNECTION" => "OK",
+            "REDIS_CONNECTION" => "OK",
+            "RABBITMQ_CONNECTION" => "OK",
+        ];
+
         $this->assertJsonStringEqualsJsonString(
-            json_encode(["API_CONNECTION" => "OK"]),
+            json_encode($expectedResponseBody),
             (string)$response->getBody(),
         );
         $this->assertSame(200, $response->getStatusCode());
@@ -45,29 +53,80 @@ class HealthCheckActionTest extends TestCase
     public function testHealthCheckActionError(): void
     {
         $app = $this->getApp();
-        /** @var Container $container */
         $container = $app->getContainer();
 
-        /** @var HealthCheckService&MockObject $healthCheckServiceMock */
         $healthCheckServiceMock = $this->createMock(HealthCheckService::class);
-        $healthCheckServiceMock
-            ->expects($this->once())
-            ->method("statusCode")
-            ->willReturn(503);
+
         $healthCheckServiceMock
             ->expects($this->once())
             ->method("statusList")
             ->willReturn([
+                "DB_CONNECTION" => "ERROR",
                 "API_CONNECTION" => "OK",
+                "REDIS_CONNECTION" => "ERROR",
+                "RABBITMQ_CONNECTION" => "OK",
             ]);
+
+        $healthCheckServiceMock
+            ->expects($this->once())
+            ->method("statusCode")
+            ->willReturn(503);
 
         $container->set(HealthCheckService::class, $healthCheckServiceMock);
 
         $request = $this->createRequest("GET", "/health-check");
         $response = $app->handle($request);
 
+        $expectedResponseBody = [
+            "DB_CONNECTION" => "ERROR",
+            "API_CONNECTION" => "OK",
+            "REDIS_CONNECTION" => "ERROR",
+            "RABBITMQ_CONNECTION" => "OK",
+        ];
+
         $this->assertJsonStringEqualsJsonString(
-            json_encode(["API_CONNECTION" => "OK"]),
+            json_encode($expectedResponseBody),
+            (string)$response->getBody(),
+        );
+        $this->assertSame(503, $response->getStatusCode());
+    }
+
+    public function testHealthCheckActionPartialError(): void
+    {
+        $app = $this->getApp();
+        $container = $app->getContainer();
+
+        $healthCheckServiceMock = $this->createMock(HealthCheckService::class);
+
+        $healthCheckServiceMock
+            ->expects($this->once())
+            ->method("statusList")
+            ->willReturn([
+                "DB_CONNECTION" => "OK",
+                "API_CONNECTION" => "OK",
+                "REDIS_CONNECTION" => "OK",
+                "RABBITMQ_CONNECTION" => "ERROR",
+            ]);
+
+        $healthCheckServiceMock
+            ->expects($this->once())
+            ->method("statusCode")
+            ->willReturn(503);
+
+        $container->set(HealthCheckService::class, $healthCheckServiceMock);
+
+        $request = $this->createRequest("GET", "/health-check");
+        $response = $app->handle($request);
+
+        $expectedResponseBody = [
+            "DB_CONNECTION" => "OK",
+            "API_CONNECTION" => "OK",
+            "REDIS_CONNECTION" => "OK",
+            "RABBITMQ_CONNECTION" => "ERROR",
+        ];
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($expectedResponseBody),
             (string)$response->getBody(),
         );
         $this->assertSame(503, $response->getStatusCode());
